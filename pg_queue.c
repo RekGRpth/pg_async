8313b6db099e0f8b89609695181add22968164f3
RekGRpth <rekgrpth@gmail.com>
2021-03-26 16:43:58 +0500

    notify

--- pg_queue.c
+++ pg_queue.c
@@ -11,6 +11,7 @@
 #include <commands/extension.h>
 #include <commands/prepare.h>
 //#include <commands/user.h>
+#include <common/hashfn.h>
 #include <common/ip.h>
 #include <executor/spi.h>
 #include <fe_utils/recovery_gen.h>
@@ -87,6 +88,26 @@
 PG_MODULE_MAGIC;
 
 #define NOTIFY_PAYLOAD_MAX_LENGTH (BLCKSZ - NAMEDATALEN - 128)
+
+typedef struct Notification {
+    uint16 channel_len; // length of channel-name string
+    uint16 payload_len; // length of payload string
+    char data[FLEXIBLE_ARRAY_MEMBER]; // null-terminated channel name, then null-terminated payload follow
+} Notification;
+
+typedef struct NotificationList {
+//    int nestingLevel; // current transaction nesting depth
+    List *events; // list of Notification structs
+    HTAB *hashtab; // hash of NotificationHash structs, or NULL
+//    struct NotificationList *upper; // details for upper transaction levels
+} NotificationList;
+
+#define MIN_HASHABLE_NOTIFIES 0 // threshold to build hashtab 16
+
+typedef struct NotificationHash {
+    Notification *event; // => the actual Notification struct
+} NotificationHash;
+
 typedef struct pg_queue_shmem_t {
     char channel[NAMEDATALEN];
     char payload[NOTIFY_PAYLOAD_MAX_LENGTH];
@@ -95,7 +116,8 @@ typedef struct pg_queue_shmem_t {
 } pg_queue_shmem_t;
 
 static int pg_queue_size;
-static List *pg_queue_channel = NIL;
+static List *listenChannels = NIL;
+static NotificationList *pendingNotifies = NULL;
 static pg_queue_shmem_t *pg_queue_shmem;
 static pqsigfunc pg_queue_signal_original = NULL;
 static shmem_startup_hook_type pg_queue_shmem_startup_hook_original = NULL;
@@ -127,7 +149,7 @@ void _PG_init(void); void _PG_init(void) {
 
 static bool pg_queue_channel_exists(const char *channel) {
     ListCell *p;
-    foreach(p, pg_queue_channel) {
+    foreach(p, listenChannels) {
         char *lchan = (char *) lfirst(p);
         if (!strcmp(lchan, channel)) return true;
     }
@@ -135,7 +157,7 @@ static bool pg_queue_channel_exists(const char *channel) {
 }
 
 static void pg_queue_signal(SIGNAL_ARGS) {
-    if (pg_queue_channel != NIL) {
+    if (listenChannels != NIL) {
         char *channel;
         char *payload;
         int32 pid;
@@ -159,7 +181,7 @@ EXTENSION(pg_queue_listen) {
     const char *channel = PG_ARGISNULL(0) ? "" : text_to_cstring(PG_GETARG_TEXT_PP(0));
     if (!pg_queue_channel_exists(channel)) {
         MemoryContext oldcontext = MemoryContextSwitchTo(TopMemoryContext);
-        pg_queue_channel = lappend(pg_queue_channel, pstrdup(channel));
+        listenChannels = lappend(listenChannels, pstrdup(channel));
         MemoryContextSwitchTo(oldcontext);
         if (!pg_queue_signal_original) pg_queue_signal_original = pqsignal(SIGUSR1, pg_queue_signal);
     }
@@ -170,8 +192,8 @@ EXTENSION(pg_queue_listening_channels) {
     FuncCallContext *funcctx;
     if (SRF_IS_FIRSTCALL()) funcctx = SRF_FIRSTCALL_INIT();
     funcctx = SRF_PERCALL_SETUP();
-    if (funcctx->call_cntr < list_length(pg_queue_channel)) {
-        char *channel = (char *)list_nth(pg_queue_channel, funcctx->call_cntr);
+    if (funcctx->call_cntr < list_length(listenChannels)) {
+        char *channel = (char *)list_nth(listenChannels, funcctx->call_cntr);
         SRF_RETURN_NEXT(funcctx, CStringGetTextDatum(channel));
     }
     SRF_RETURN_DONE(funcctx);
@@ -186,15 +208,96 @@ static void pg_queue_kill(void) {
     }
 }
 
+static bool AsyncExistsPendingNotify(Notification *n) {
+    if (!pendingNotifies) return false;
+    if (pendingNotifies->hashtab) {
+        if (hash_search(pendingNotifies->hashtab, &n, HASH_FIND, NULL)) return true;
+    } else {
+        ListCell *l;
+        foreach(l, pendingNotifies->events) {
+            Notification *oldn = (Notification *)lfirst(l);
+            if (n->channel_len == oldn->channel_len && n->payload_len == oldn->payload_len && !memcmp(n->data, oldn->data, n->channel_len + n->payload_len + 2)) return true;
+        }
+    }
+    return false;
+}
+
+static uint32 notification_hash(const void *key, Size keysize) {
+    const Notification *k = *(const Notification *const *)key;
+    Assert(keysize == sizeof(Notification *));
+    return DatumGetUInt32(hash_any((const unsigned char *)k->data, k->channel_len + k->payload_len + 1));
+}
+
+static int notification_match(const void *key1, const void *key2, Size keysize) {
+    const Notification *k1 = *(const Notification *const *)key1;
+    const Notification *k2 = *(const Notification *const *)key2;
+    Assert(keysize == sizeof(Notification *));
+    if (k1->channel_len == k2->channel_len && k1->payload_len == k2->payload_len && !memcmp(k1->data, k2->data, k1->channel_len + k1->payload_len + 2)) return 0; // equal
+    return 1; // not equal
+}
+
+static void AddEventToPendingNotifies(Notification *n) {
+    Assert(pendingNotifies->events != NIL);
+    if (list_length(pendingNotifies->events) >= MIN_HASHABLE_NOTIFIES && !pendingNotifies->hashtab) {
+        HASHCTL hash_ctl;
+        ListCell *l;
+        MemSet(&hash_ctl, 0, sizeof(hash_ctl));
+        hash_ctl.keysize = sizeof(Notification *);
+        hash_ctl.entrysize = sizeof(NotificationHash);
+        hash_ctl.hash = notification_hash;
+        hash_ctl.match = notification_match;
+        hash_ctl.hcxt = CurTransactionContext;
+        pendingNotifies->hashtab = hash_create("Pending Notifies", 256L, &hash_ctl, HASH_ELEM | HASH_FUNCTION | HASH_COMPARE | HASH_CONTEXT);
+        foreach(l, pendingNotifies->events) {
+            Notification *oldn = (Notification *)lfirst(l);
+            bool found;
+            NotificationHash *hentry = hash_search(pendingNotifies->hashtab, &oldn, HASH_ENTER, &found);
+            Assert(!found);
+            hentry->event = oldn;
+        }
+    }
+    pendingNotifies->events = lappend(pendingNotifies->events, n);
+    if (pendingNotifies->hashtab) {
+        bool found;
+        NotificationHash *hentry = hash_search(pendingNotifies->hashtab, &n, HASH_ENTER, &found);
+        Assert(!found);
+        hentry->event = n;
+    }
+}
+
 EXTENSION(pg_queue_notify) {
-    text *channel = PG_ARGISNULL(0) ? NULL : DatumGetTextP(PG_GETARG_DATUM(0));
-    text *payload = PG_ARGISNULL(1) ? NULL : DatumGetTextP(PG_GETARG_DATUM(1));
+    const char *channel = PG_ARGISNULL(0) ? "" : text_to_cstring(PG_GETARG_TEXT_PP(0));
+    const char *payload = PG_ARGISNULL(1) ? "" : text_to_cstring(PG_GETARG_TEXT_PP(1));
+    size_t channel_len = PG_ARGISNULL(0) ? 0 : strlen(channel);
+    size_t payload_len = PG_ARGISNULL(1) ? 0 : strlen(payload);
+    MemoryContext oldcontext;
+    Notification *n;
     if (IsParallelWorker()) elog(ERROR, "cannot send notifications from a parallel worker");
+    if (!channel_len) ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("channel name cannot be empty")));
+    if (channel_len >= NAMEDATALEN) ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("channel name too long")));
+    if (payload_len >= NOTIFY_PAYLOAD_MAX_LENGTH) ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("payload string too long")));
+    oldcontext = MemoryContextSwitchTo(CurTransactionContext);
+    n = palloc(offsetof(Notification, data) + channel_len + payload_len + 2);
+    n->channel_len = channel_len;
+    n->payload_len = payload_len;
+    strcpy(n->data, channel);
+    if (payload) strcpy(n->data + channel_len + 1, payload);
+    else n->data[channel_len + 1] = '\0';
+    if (!pendingNotifies) {
+        pendingNotifies = MemoryContextAlloc(TopTransactionContext, sizeof(*pendingNotifies));
+//        pendingNotifies->nestingLevel = my_level;
+        pendingNotifies->events = list_make1(n);
+        pendingNotifies->hashtab = NULL;
+//        pendingNotifies->upper = pendingNotifies;
+    } else {
+        if (AsyncExistsPendingNotify(n)) pfree(n);
+        else AddEventToPendingNotifies(n);
+    }
+    MemoryContextSwitchTo(oldcontext);
     SpinLockAcquire(&pg_queue_shmem->mutex);
-    if (channel) memcpy(pg_queue_shmem->channel, VARDATA_ANY(channel), Min(NAMEDATALEN - 1, VARSIZE_ANY_EXHDR(channel)));
-    pg_queue_shmem->channel[channel ? Min(NAMEDATALEN - 1, VARSIZE_ANY_EXHDR(channel)) : sizeof("") - 1] = '\0';
-    if (payload) memcpy(pg_queue_shmem->payload, VARDATA_ANY(payload), Min(NOTIFY_PAYLOAD_MAX_LENGTH - 1, VARSIZE_ANY_EXHDR(payload)));
-    pg_queue_shmem->payload[payload ? Min(NOTIFY_PAYLOAD_MAX_LENGTH - 1, VARSIZE_ANY_EXHDR(payload)) : sizeof("") - 1] = '\0';
+    strcpy(pg_queue_shmem->channel, channel);
+    if (payload) strcpy(pg_queue_shmem->payload, payload);
+    else pg_queue_shmem->payload[0] = '\0';
     pg_queue_shmem->pid = MyProcPid;
     SpinLockRelease(&pg_queue_shmem->mutex);
     pg_queue_kill();
@@ -202,8 +305,8 @@ EXTENSION(pg_queue_notify) {
 }
 
 EXTENSION(pg_queue_unlisten_all) {
-    list_free_deep(pg_queue_channel);
-    pg_queue_channel = NIL;
+    list_free_deep(listenChannels);
+    listenChannels = NIL;
     if (pg_queue_signal_original) {
         pqsignal(SIGUSR1, pg_queue_signal_original);
         pg_queue_signal_original = NULL;
@@ -214,15 +317,15 @@ EXTENSION(pg_queue_unlisten_all) {
 EXTENSION(pg_queue_unlisten) {
     const char *channel = PG_ARGISNULL(0) ? "" : text_to_cstring(PG_GETARG_TEXT_PP(0));
     ListCell *q;
-    foreach(q, pg_queue_channel) {
+    foreach(q, listenChannels) {
         char *lchan = (char *)lfirst(q);
         if (!strcmp(lchan, channel)) {
-            pg_queue_channel = foreach_delete_current(pg_queue_channel, q);
+            listenChannels = foreach_delete_current(listenChannels, q);
             pfree(lchan);
             break;
         }
     }
-    if (!list_length(pg_queue_channel) && pg_queue_signal_original) {
+    if (!list_length(listenChannels) && pg_queue_signal_original) {
         pqsignal(SIGUSR1, pg_queue_signal_original);
         pg_queue_signal_original = NULL;
     }
