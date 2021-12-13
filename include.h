de98cd818d2ad2a9244a566b1320a88f2b407080
RekGRpth <rekgrpth@gmail.com>
2021-12-13 15:30:13 +0500

    debug

--- include.h
+++ include.h
@@ -1,18 +1,6 @@
 #ifndef _INCLUDE_H_
 #define _INCLUDE_H_
 
-#define D1(...) ereport(DEBUG1, (errmsg(__VA_ARGS__)))
-#define D2(...) ereport(DEBUG2, (errmsg(__VA_ARGS__)))
-#define D3(...) ereport(DEBUG3, (errmsg(__VA_ARGS__)))
-#define D4(...) ereport(DEBUG4, (errmsg(__VA_ARGS__)))
-#define D5(...) ereport(DEBUG5, (errmsg(__VA_ARGS__)))
-#define E(...) ereport(ERROR, (errmsg(__VA_ARGS__)))
-#define F(...) ereport(FATAL, (errmsg(__VA_ARGS__)))
-#define I(...) ereport(INFO, (errmsg(__VA_ARGS__)))
-#define L(...) ereport(LOG, (errmsg(__VA_ARGS__)))
-#define N(...) ereport(NOTICE, (errmsg(__VA_ARGS__)))
-#define W(...) ereport(WARNING, (errmsg(__VA_ARGS__)))
-
 #define countof(array) (sizeof(array)/sizeof(array[0]))
 
 #include <postgres.h>
